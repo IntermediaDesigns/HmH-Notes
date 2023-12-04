77f756f16ef452a4cfb390b11f659fe68cafc4e6
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-12-04 10:02:46 -0500

    update

--- Fetch/RESTwithGETexample.js
+++ Fetch/RESTwithGETexample.js
@@ -11,14 +11,14 @@ export default function Users() {
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setUsers(data));
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
   }, []);
 
   return (
     <div>
       <h1>Users</h1>
-      {users.map(user => (
+      {users.map((user) => (
         <div key={user.id}>
           <h2>{user.name}</h2>
           <p>{user.email}</p>
